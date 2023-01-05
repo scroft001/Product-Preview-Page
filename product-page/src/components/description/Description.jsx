60c42fdf1d8492ac82f3ba854ce64a8811d5dec3
Steven <scroft001@gmail.com>
2023-01-04 17:13:33 -0800

    Added description jsx

--- product-page/src/components/description/Description.jsx
+++ product-page/src/components/description/Description.jsx
@@ -3,8 +3,11 @@ import './description.scss'
 
 const Description = () => {
   return (
-    <div>
-      Description
+    <div className='app__description'>
+      <h3>Perfume</h3>
+      <h1>Gabrielle Essence Eau De Parfum</h1>
+      <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, 
+  Perfumer-Creator for the House of CHANEL.</p>
     </div>
   )
 }
