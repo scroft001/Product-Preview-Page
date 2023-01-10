a84a06d63de445b2c517772f8fe11a87df742b1d
Steven <scroft001@gmail.com>
2023-01-09 17:17:24 -0800

    Buy button finished

--- product-page/src/components/buyButton/BuyButton.jsx
+++ product-page/src/components/buyButton/BuyButton.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './buyButton.scss'
+import cart from '../../images/icon-cart.svg'
 
 const BuyButton = () => {
   return (
-    <div>BuyButton</div>
+    <div className='app__buy'>
+      <img src={cart} alt='cart' />
+      <p>Add to Cart</p>
+    </div>
   )
 }
 
